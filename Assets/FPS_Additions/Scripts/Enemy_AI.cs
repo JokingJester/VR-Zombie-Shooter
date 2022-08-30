aa05aaf7763e3d5dd680ed926dc01bde0d3aad82
David Brown <81639209+JokingJester@users.noreply.github.com>
2022-08-30 12:29:04 -0500

    Dowloaded Filebase + FPS Shooter Assets
    Downloaded the zombie FPS game from filebase.

---
+++ Assets/FPS_Additions/Scripts/Enemy_AI.cs
@@ -0,0 +1,89 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy_AI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Speed;
+    public bool YouDied;
+    public bool ChasePlayer;
+
+
+    private NavMeshAgent _agent;
+    [SerializeField]
+    private Transform _target;
+    [SerializeField]
+    private Animator _anim;
+    [SerializeField]
+    private Transform _player;
+    [SerializeField]
+    private float _playerDistance;
+ 
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.speed = Speed;
+        _anim = transform.Find("Man_01_Dismembered").GetComponent<Animator>();
+        _target = GameObject.Find("Player").GetComponent<Transform>();
+        _player = GameObject.Find("Player").GetComponent<Transform>();
+        _agent.avoidancePriority = Random.Range(0, 50);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (Vector3.Distance(_player.transform.position, this.transform.position) < _playerDistance)
+        {
+            
+            ChasePlayer = true;
+        }
+
+        if (ChasePlayer == true)
+        {
+            _agent.SetDestination(_target.position);
+        }
+
+        if (_agent.velocity.magnitude > 0.1f)
+        {
+            _anim.SetBool("Walk", true);
+        }
+        
+        if (_agent.velocity.magnitude < 0.1f)
+        {
+            _anim.SetBool("Walk", false);
+        }
+
+        if (_agent.remainingDistance < 1.5f)
+        {
+            _anim.SetBool("Attack", true);
+            _agent.speed = 0;
+        }
+
+        if (_agent.remainingDistance >= 1.5f)
+        {
+            _anim.SetBool("Attack", false);
+            _agent.speed = Speed; 
+        }
+
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Zombie_Death"))
+        {
+            Speed = 0;
+            StartCoroutine(DestroyZombie());
+        }
+
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Zombie_Death_Prone"))
+        {
+            Speed = 0;
+            StartCoroutine(DestroyZombie());
+        }
+    }
+
+    IEnumerator DestroyZombie()
+    {
+        yield return new WaitForSeconds(10.0f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/GameDevHQ/FileBase/3D/Props/Weapons/USP/Additional_Gun_Folder/Scripts/Gun_Fire_Pistol.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gun_Fire_Pistol : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem _smoke;
+
+    [SerializeField]
+    private ParticleSystem _bulletCasing;
+
+    [SerializeField]
+    private ParticleSystem _muzzleFlashSide;
+
+    [SerializeField]
+    private ParticleSystem _Muzzle_Flash_Front;
+
+    private Animator _anim;
+
+    [SerializeField]
+    private AudioClip _gunShotAudioClip;
+
+    [SerializeField]
+    private AudioClip _reloadAudioClip;
+
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    private Raycast _raycast;
+
+    [SerializeField]
+    private int _clipSize;//total amount of bullets inside each clip
+    public int _remainingBulletsInClip;//total amount of bullets inside my clip
+    public int totalBullets;//total amount of bullets carried
+    public int maxBulletsAvailable; //cap of how many bullets can be carried
+    private int _bullet_Refill_Amount;// amount of bullets added when picking up ammo
+    [SerializeField]
+
+    private bool _reload;
+    private bool _reloadCooldown;
+
+    [SerializeField]
+    private Text _totalBulletInInventory;
+
+    [SerializeField]
+    private Text _ammoTextHolder;
+    public bool FullAuto;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _raycast = GetComponent<Raycast>();
+        _ammoTextHolder = GameObject.Find("Ammo_Count").GetComponent<Text>();
+        _totalBulletInInventory = GameObject.Find("Total_Bullets_Inventory").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_remainingBulletsInClip >= 1 && _reloadCooldown == false)// checks if the player has bullets in the gun
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                if (FullAuto == false)
+                {
+                    _anim.SetTrigger("Fire");
+                }
+
+                if (FullAuto == true)
+                {
+                    _anim.SetBool("Automatic_Fire", true);
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                if (FullAuto == true)
+                {
+                    _anim.SetBool("Automatic_Fire", false);
+                }
+
+                if (FullAuto == false)
+                {
+                    _anim.SetBool("Fire", false);
+                }
+            }
+        }
+        if (_remainingBulletsInClip < 1)
+        {
+            _anim.SetBool("Automatic_Fire", false);
+            ReloadWeapon();
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadWeapon();
+        }
+        BulletCount();
+        _ammoTextHolder.text = _remainingBulletsInClip.ToString() + " / " + _clipSize.ToString();
+    }
+
+
+    public void FireGunParticles()
+    {
+        Debug.Log("Fired gun particles");
+        _smoke.Play();
+        _bulletCasing.Play();
+        _muzzleFlashSide.Play();
+        _Muzzle_Flash_Front.Play();
+        GunFireAudio();
+        _raycast.Shoot();
+        _remainingBulletsInClip -= 1;
+        
+    }
+
+    public void GunFireAudio()
+    {
+        _audioSource.pitch = Random.Range(0.9f, 1.1f);
+        _audioSource.PlayOneShot(_gunShotAudioClip);
+    }
+
+    public void ReloadAudio()
+    {
+        _audioSource.pitch = Random.Range(0.9f, 1.1f);
+        _audioSource.PlayOneShot(_reloadAudioClip);
+    }
+
+    IEnumerator WeaponCoolDown()// reload cooldown 
+    {
+        _reloadCooldown = true;
+        yield return new WaitForSeconds(1.25f);
+        _reloadCooldown = false;
+    }
+
+    public void ReloadWeapon()
+    {
+        if (totalBullets <= 0)// you have no more bullets
+        {
+            //play empty bullet firing animation - need to create
+            //no reload allowed
+
+        }
+        if (totalBullets > 0)//If you can reload
+        {
+            int bulletCount;
+            bulletCount = _clipSize - _remainingBulletsInClip;                           
+            _remainingBulletsInClip += bulletCount;
+            totalBullets -= bulletCount;//remove bullets from inventory and place into clip
+            if (totalBullets < 0)//If you reload with a negative total bullet count
+            {
+                int negative_bullet_count;
+                negative_bullet_count = totalBullets;
+                _remainingBulletsInClip += negative_bullet_count;
+                totalBullets = 0;//Fixes count so remainder of bullets go into clip and sets total to 0
+            }
+            _anim.SetTrigger("Reload");
+            StartCoroutine(WeaponCoolDown());
+        }        
+    }
+
+    public void AddBullets()
+    {
+        int temp_bullet_amt;
+        temp_bullet_amt = totalBullets + _bullet_Refill_Amount;
+
+        if (temp_bullet_amt > maxBulletsAvailable)// if you are collecting more bullets than your max
+        {
+            totalBullets = maxBulletsAvailable;//Just set your bullet count to max
+        }
+
+        else
+        {
+            totalBullets = totalBullets + _bullet_Refill_Amount;//if you are collecting less than your max, just add it
+        }
+    }
+
+    public void BulletCount()
+    {
+        int tempTotal;
+        tempTotal = totalBullets + _remainingBulletsInClip;
+        _totalBulletInInventory.text = "Ammo:" + tempTotal.ToString();
+    }
+}
